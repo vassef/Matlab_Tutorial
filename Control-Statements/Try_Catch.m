460efae7ed14d49728030780c864a53757391f52
Shayan <shayanvassef2000@gmail.com>
2022-06-28 20:19:38 +0430

    Add files via upload

---
+++ Control-Statements/Try_Catch.m
@@ -0,0 +1,20 @@
+%% Try_Catch
+
+
+%% try-catch
+
+% used to ignore errors but collect information about the error for later inspection
+
+vec = [2 3 4];
+
+% case 1:
+vec(4)
+
+% case 2:
+try
+    vec(4)
+catch me
+end
+
+
+%% done.
